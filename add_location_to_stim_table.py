4bfca76b9c81da459cfc216947154b196ee268a3
Zsuzsanna Nemecz <zsuzska.nemecz@gmail.com>
2020-03-02 17:22:58 +0100

    switched to .at for indexing within dataframes. added discrete locations

--- add_location_to_stim_table.py
+++ add_location_to_stim_table.py
@@ -1,35 +1,37 @@
 import pandas as pd
 import numpy as np
 import random
-fname ='StimuliTable-Encoding-3run-40-520-12345.csv'
-stim_table = pd.read_csv(fname, sep=',', lineterminator='\n')
-coord = pd.read_csv('coordinate_table_discrete_1.csv')
+fname ='StimuliTable-Encoding-3run-40-520-12345.xlsx'
+stim_table = pd.read_excel(fname)
+#stim_table[stim_table.columns[-1]].str.strip()
+print(stim_table)
+coord = pd.read_excel('coordinate_table_discrete_1.xlsx')
 i = 0
 for row in range(len(stim_table)):
     if stim_table['Order'][row] == 1:
         delay = stim_table['Delay'][row] + 1
-        stim_table['Xcoordinate'][row] = coord['Xcoordinate'][i]
-        stim_table['Ycoordinate'][row] = coord['Ycoordinate'][i]
-        stim_table['Xcoordinate'][row + delay] = coord['Xcoordinate'][i]
-        stim_table['Ycoordinate'][row+delay] = coord['Ycoordinate'][i]
+        stim_table.at[row, 'Xcoordinate'] = coord.at[i, 'Xcoordinate']
+        stim_table.at[row, 'Ycoordinate'] = coord.at[i, 'Ycoordinate']
+        stim_table.at[row+delay, 'Xcoordinate'] = coord.at[i,'Xcoordinate']
+        stim_table.at[row+delay,'Ycoordinate'] = coord.at[i, 'Ycoordinate']
 
-        stim_table['Xcoordinate_lure1'][row] = coord['Xcoordinate_lure1'][i]
-        stim_table['Ycoordinate_lure1'][row] = coord['Ycoordinate_lure1'][i]
-        stim_table['Xcoordinate_lure1'][row+delay] = coord['Xcoordinate_lure1'][i]
-        stim_table['Ycoordinate_lure1'][row+delay] = coord['Ycoordinate_lure1'][i]
-
-
-        stim_table['Xcoordinate_lure2'][row] = coord['Xcoordinate_lure2'][i]
-        stim_table['Ycoordinate_lure2'][row] = coord['Ycoordinate_lure2'][i]
-        stim_table['Xcoordinate_lure2'][row+delay] = coord['Xcoordinate_lure2'][i]
-        stim_table['Ycoordinate_lure2'][row+delay] = coord['Ycoordinate_lure2'][i]
+        stim_table.at[row, 'Xcoordinate_lure1']= coord.at[i, 'Xcoordinate_lure1']
+        stim_table.at[row, 'Ycoordinate_lure1'] = coord.at[i, 'Ycoordinate_lure1']
+        stim_table.at[row+delay, 'Xcoordinate_lure1'] = coord.at[i, 'Xcoordinate_lure1']
+        stim_table.at[row+delay, 'Ycoordinate_lure1']= coord.at[i, 'Ycoordinate_lure1']
 
 
-        stim_table['Xcoordinate_foil'][row] = coord['Xcoordinate_foil'][i]
-        stim_table['Ycoordinate_foil'][row] = coord['Ycoordinate_foil'][i]
-        stim_table['Xcoordinate_foil'][row+delay] = coord['Xcoordinate_foil'][i]
-        stim_table['Ycoordinate_foil'][row+delay] = coord['Ycoordinate_foil'][i]
+        stim_table.at[row, 'Xcoordinate_lure2']= coord.at[i, 'Xcoordinate_lure2']
+        stim_table.at[row, 'Ycoordinate_lure2'] = coord.at[i, 'Ycoordinate_lure2']
+        stim_table.at[row+delay, 'Xcoordinate_lure2'] = coord.at[i, 'Xcoordinate_lure2']
+        stim_table.at[row+delay, 'Ycoordinate_lure2'] = coord.at[i, 'Ycoordinate_lure2']
+
+
+        stim_table.at[row, 'Xcoordinate_foil'] = coord.at[i, 'Xcoordinate_foil']
+        stim_table.at[row, 'Ycoordinate_foil'] = coord.at[i, 'Ycoordinate_foil']
+        stim_table.at[row+delay, 'Xcoordinate_foil'] = coord.at[i, 'Xcoordinate_foil']
+        stim_table.at[row+delay, 'Ycoordinate_foil'] = coord.at[i, 'Ycoordinate_foil']
 
         i +=1
 
-stim_table.to_csv('StimuliTable-Encoding-3run-40-520-12345_new_coord.csv')
+stim_table.to_excel('StimuliTable-Encoding-3run-40-discrete-8-12345_new_coord.xlsx')
