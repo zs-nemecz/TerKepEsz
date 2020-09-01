20c25bd65ff44705ccb40b0573dd5c8e1d3564e2
Zsuzsanna Nemecz <nemecz.zsuzsanna@ttk.hu>
2020-09-01 17:58:34 +0200

    preparing code for assigning stimuli blocks

---
+++ create_stimuli_tables.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import numpy.random as rd
+
+# todo
+# define tests for: checking matche between trial pairs; encoding and recognition block matching, number of trials/block
+# create classes for experiment, blocks and trials
+# create pandas dataframe and save csv
